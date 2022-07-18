0be14f9786886f8c738f3aa653ad4cfb4759bce7
prabesh-np <prabeshthapa.npofficial@gmail.com>
2022-07-18 09:34:44 +0545

    Images converted to .webp format

--- js/script.js
+++ js/script.js
@@ -1,10 +1,10 @@
-const btn = document.getElementById('menu-btn')
-const menu = document.getElementById('menu')
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
 
-btn.addEventListener('click', navToggle)
+btn.addEventListener("click", navToggle);
 
 function navToggle() {
-  btn.classList.toggle('open')
-  menu.classList.toggle('flex')
-  menu.classList.toggle('hidden')
+   btn.classList.toggle("open");
+   menu.classList.toggle("flex");
+   menu.classList.toggle("hidden");
 }
